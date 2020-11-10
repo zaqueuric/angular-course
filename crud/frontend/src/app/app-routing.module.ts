d35d0da05ae116cece19fdb931d7ce52c4e04818
Zaqueu Ricardo <zaqueu.ricardo@luminae.com.br>
2020-11-10 00:33:39 -0200

    Insert navegation

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+import { from } from 'rxjs';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
